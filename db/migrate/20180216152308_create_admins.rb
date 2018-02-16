05859a111fddf5c31db44d2097070d3c72d57f72
SadekAlshaar018 <sadekalshaar018@gmail.com>
2018-02-16 18:55:09 +0100

    Update
    config/database.yml update a file

--- db/migrate/20180216152308_create_admins.rb
+++ db/migrate/20180216152308_create_admins.rb
@@ -2,10 +2,10 @@ class CreateAdmins < ActiveRecord::Migration[5.1]
   def change
     create_table :admins do |t|
 
-      t.string "first_name" :limit => 30
+      t.string "first_name",:limit => 30
       t.string "last name", :limit => 30
       t.string "email", :default => '', :null => false
-      t.string "password" ,:limit => 40
+      t.string "password",:limit => 40
       t.timestamps
 
     end
